6ad884d84f5c3996dda22a478a4c07c2b5dcdb52
Lucy Liu <lucycl@uci.edu>
2025-03-15 01:04:46 +0000

    dijkstras modified

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -7,7 +7,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     vector<bool> visited(numVertices, false);
     distances[source] = 0;
     previous[source] = -1;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minheap; // <vertex, weight>
+    priority_queue<pair<int, int>> minheap; // <vertex, weight>
     minheap.push({source, 0});
     while(!minheap.empty())
     {
